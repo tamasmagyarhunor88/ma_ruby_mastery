4dbf271639ec46e4e94157f34aee7fc1ff437bbd
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2017-12-31 18:53:44 +0200

    add first while code

---
+++ while_exec.rb
@@ -0,0 +1,14 @@
+# we set our variable to 10
+number = 10
+
+# we ask our while loop to run following commands while conditions are true
+# while checks if our variable is more or equal than 10 and less or equal than
+# 100 and while they are true, it keeps running the loop
+while number >= 10 && number <= 100
+  # while they are true, the loop puts number
+  puts number
+  # while they are true, the loop also increases number with one and saves
+  # it back to the variable number
+  number += 1
+  # ends the loop
+end
