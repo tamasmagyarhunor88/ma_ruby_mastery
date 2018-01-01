31cfc7ee22cd329b90a18fcfafb826b69277e8ac
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-01-01 09:45:14 +0200

    add comments to while_exer2.rb@vim

--- while_exer2.rb
+++ while_exer2.rb
@@ -1,10 +1,20 @@
+# comments done in vim
+# set variable to 10
 number = 10
 
+# defining while loop, keep doing it while number is bigger
+# or equal than 10
 while number >= 10
+# puts number  
   puts number
+# increment number variable with 1 ( = add 1 to it and save it
+# as well
   number += 1
-
+# we condition our while loop, if the number is bigger than
+# 100
   if number > 100
+# break it, stop the whole while loop
     break
+# end the if loop and the while loop
   end
 end
