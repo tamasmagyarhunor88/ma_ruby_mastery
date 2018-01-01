6da76b4cac661c648036f9691c51d450b08c7c27
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-01-01 10:19:48 +0200

    add 2 exercise

---
+++ odd_even.rb
@@ -0,0 +1,17 @@
+# set variable
+number = 1
+
+# keep while loop running while humber is less than 101
+while number < 101 do
+# if number.even? 
+# if the odd? condition is true on number? if the number is odd? ..
+  if number.odd?
+# print the number
+    puts number
+# end the if conditional
+  end
+
+# increment number variable by 1
+  number += 1
+# end the code
+end
---
+++ return_exer.rb
@@ -0,0 +1,15 @@
+# USE VIM FOR COMMENTS !!!
+# set condition to true
+if true
+# add value to variable
+  a = 1
+# return the variable and finish the condition
+  return a
+
+# add value to variable  
+  b = 2
+# return will never execute since the first return
+# closed the loop
+  return b
+# end code
+end
