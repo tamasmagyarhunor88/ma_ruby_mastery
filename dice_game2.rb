75134131a2e11466fb2a88097aec2ff4b4a9f37b
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-01-01 11:01:25 +0200

    comments added

--- dice_game2.rb
+++ dice_game2.rb
@@ -1,13 +1,15 @@
-# both sides dice
+# first round
 player_me = rand(1..6)
 player_comp = rand(1..6)
+# second round
+player_me += rand(1..6)
+player_comp += rand(1..6)
 
 # no equal game
 while player_me == player_comp
   player_me = rand(1..6)
   player_comp = rand(1..6)
 end
-
 # print results
 if player_me > player_comp
   puts true , "You diced #{player_me} and Computer diced #{player_comp} !"
