dc38a7b6fb4f07f40f8f2b0004fe7bc8e9ad09a2
Magyar-Hunor Tamas <tamasmagyarhunor88@gmail.com>
2018-01-17 22:54:32 +0000

    methods exer average

---
+++ average.rb
@@ -0,0 +1,20 @@
+def average(scores)
+  scores_accumulator = 0
+
+  scores.each do |score|
+    scores_accumulator += score
+  end
+
+  result = scores_accumulator.to_f / scores.length
+end
+
+# Here are the scores
+test_scores_for_class_1 = [55, 78, 67, 92]
+test_scores_for_class_2 = [48, 99, 91, 70]
+test_scores_for_class_3 = [56, 58, 61, 98, 100]
+
+puts average(test_scores_for_class_1)
+puts average(test_scores_for_class_2)
+puts average(test_scores_for_class_3)
+
+puts average([average(test_scores_for_class_1), average(test_scores_for_class_2), average(test_scores_for_class_3)])
